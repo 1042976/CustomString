ad968e9948d81b59305ceefbf192edbc89cec6ec
Xiaodong Liu <xiaodongliu@xiaodongs-mbp.lan>
2022-08-20 22:05:03 -0400

    rm

---
+++ CustomStringTest.cpp
@@ -0,0 +1,13 @@
+
+#include "../CustomString.h"
+#include "gtest/gtest.h"
+//namespace {
+//class CustomStringTest: public :: testing::Test{
+//
+//};
+//}
+
+TEST(String, HelloWorld){
+    int i = 1;
+    EXPECT_EQ(i, 1);
+}
